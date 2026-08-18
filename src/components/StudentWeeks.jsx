25d297c299cf27c1dc17eff2b238eac9b98d6e45
seja2b <seja2b.br@gmail.com>
2026-08-18 18:26:33 -0300

    feat: apply calendar week styles

--- src/components/StudentWeeks.jsx
+++ src/components/StudentWeeks.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useMemo, useState } from 'react'
 import api from '../services/api'
 import './StudentWeeks.css'
+import './StudentWeeksCalendar.css'
 
 const dateFormatter = new Intl.DateTimeFormat('pt-BR', {
   timeZone: 'America/Sao_Paulo',
